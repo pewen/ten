18af7617fe3f9efdf4522ab0c2744cd95088d51b
lucas <lucas@ein>
2015-05-06 03:33:22 -0300

    Now pothon it a class and not a metod

--- nanoparticle.py
+++ nanoparticle.py
@@ -49,23 +49,34 @@ class NanoParticle(object):
             self.acceptors_positions[i][0] = np.random.uniform(low=-self.R, high=self.R)
             self.acceptors_positions[i][1] = np.random.uniform(low=-self.R, high=self.R)
             self.acceptors_positions[i][2] = np.random.uniform(low=-self.R, high=self.R)
-            
-    def photon(self):
+
+class Photon():
+    def __init__(self, nanoparticle):
         """
-        Generate random position of a photon in the nanoparticle.
+        Create a photon object, whit a random position in the nanoparticle
+
+        Parameters
+        ----------
+        nanoparticle : NanoParticle
+        
         """
-        x = np.random.uniform(low=-self.R, high=self.R)
-        y = np.random.uniform(low=-self.R, high=self.R)
-        z = np.random.uniform(low=-self.R, high=self.R)
+        x = np.random.uniform(low=-nanoparticle.R, high=nanoparticle.R)
+        y = np.random.uniform(low=-nanoparticle.R, high=nanoparticle.R)
+        z = np.random.uniform(low=-nanoparticle.R, high=nanoparticle.R)
         self.photon = np.array([x, y, z])
             
     def walk(self):
         pass
     
-    def distance(self):
+    def distance(self, nanoparticle):
         """
         Calculate, for all acceptor, 1/(r**6), where r are the distace bewteen the photon and acceptors
+
+        Parameters
+        ----------
+        nanoparticle : NanoParticle
+        
         """
-        self.dist = np.zeros(self.n_acceptors)
-        for i in range(self.n_acceptors):
-            self.dist[i] = (sum((self.photon - self.acceptors_positions[i])**2))**3
+        self.dist = np.zeros(nanoparticle.n_acceptors)
+        for i in range(nanoparticle.n_acceptors):
+            self.dist[i] = (sum((self.photon - nanoparticle.acceptors_positions[i])**2))**3
