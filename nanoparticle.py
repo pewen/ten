f4127a7c27954b3de43f36932f49836b92286cab
Franco N. Bellomo <fnbellomo@gmail.com>
2015-05-06 14:33:10 -0300

    different classes in different files

--- nanoparticle.py
+++ nanoparticle.py
@@ -49,34 +49,3 @@ class NanoParticle(object):
             self.acceptors_positions[i][0] = np.random.uniform(low=-self.R, high=self.R)
             self.acceptors_positions[i][1] = np.random.uniform(low=-self.R, high=self.R)
             self.acceptors_positions[i][2] = np.random.uniform(low=-self.R, high=self.R)
-
-class Photon():
-    def __init__(self, nanoparticle):
-        """
-        Create a photon object, whit a random position in the nanoparticle
-
-        Parameters
-        ----------
-        nanoparticle : NanoParticle
-        
-        """
-        x = np.random.uniform(low=-nanoparticle.R, high=nanoparticle.R)
-        y = np.random.uniform(low=-nanoparticle.R, high=nanoparticle.R)
-        z = np.random.uniform(low=-nanoparticle.R, high=nanoparticle.R)
-        self.photon = np.array([x, y, z])
-            
-    def walk(self):
-        pass
-    
-    def distance(self, nanoparticle):
-        """
-        Calculate, for all acceptor, 1/(r**6), where r are the distace bewteen the photon and acceptors
-
-        Parameters
-        ----------
-        nanoparticle : NanoParticle
-        
-        """
-        self.dist = np.zeros(nanoparticle.n_acceptors)
-        for i in range(nanoparticle.n_acceptors):
-            self.dist[i] = (sum((self.photon - nanoparticle.acceptors_positions[i])**2))**3
