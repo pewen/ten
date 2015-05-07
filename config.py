66dc080343c4fe3f0c40d69bad3a6933725568c4
Franco N. Bellomo <fnbellomo@gmail.com>
2015-05-07 15:25:07 -0300

    example config

---
+++ config.py
@@ -0,0 +1,27 @@
+"""
+Configuration of the problem.
+
+Parameters
+----------
+r : float
+    Nanoparticle radius. [r] = nm
+Fr : float
+    Forster radius. [Fr] = nm
+L_D : float
+    Length  of exciton diffusion. [L_D] = nm
+tau_D : float
+    Lifetime of donor. [tau_D] =
+num_acceptors : float
+    Number of acceptors in the nanoparticle.
+delta_t : float
+    Time interval
+num_exc :float
+    Numbers of exitation of the same nanoparticle
+"""
+
+r = 50
+Fr = 2.29
+L_D = 8
+tau_D = 344
+num_acceptors = 50
+delta_t = 1
