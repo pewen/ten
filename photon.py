415d484dd3603601b69cf5e340952f9424100afa
Franco N. Bellomo <fnbellomo@gmail.com>
2015-05-07 12:40:32 -0300

    add P_ET method

--- photon.py
+++ photon.py
@@ -11,14 +11,14 @@ class Photon():
         nanoparticle : object
             Nanoparticle objects
         """
-        self.np = nanoparticle
+        self.NP = nanoparticle
         
         a = 1
         while a == 1:
-            x = np.random.uniform(low = -self.np.R, high = self.np.R)
-            y = np.random.uniform(low = -self.np.R, high = self.np.R)
-            z = np.random.uniform(low = -self.np.R, high = self.np.R)
-            if x*x + y*y + z*z < self.np.R*self.np.R:
+            x = NP.random.uniform(low = -self.NP.R, high = self.NP.R)
+            y = NP.random.uniform(low = -self.NP.R, high = self.NP.R)
+            z = NP.random.uniform(low = -self.NP.R, high = self.NP.R)
+            if x*x + y*y + z*z <= self.NP.R*self.NP.R:
                 a = 0
         self.photon = np.array([x, y, z])
             
@@ -32,19 +32,31 @@ class Photon():
             theta = 2*np.pi*np.random.uniform()
             phi = np.arccos(2*np.random.uniform - 1)
         
-            new_R[0] = np.sin(phi)*np.cos(theta)*self.np.epsilon
-            new_R[1] = np.sin(phi)*np.sin(theta)*self.np.epsilon
-            new_R[2] = np.cos(phi)*self.np.epsilon
+            new_R[0] = np.sin(phi)*np.cos(theta)*self.NP.epsilon
+            new_R[1] = np.sin(phi)*np.sin(theta)*self.NP.epsilon
+            new_R[2] = np.cos(phi)*self.NP.epsilon
 
-            if (new_R + self.photon)**2 < self.photon.R*self.photon.R:
+            if sum((new_R + self.photon)**2) <= self.photon.R*self.photon.R:
                 a = 0
 
         self.photon += new_R
     
-    def distance(self):
+    def P_ET(self):
         """
-        Return the array 'dist' where each component is the account 1/(r[i]**6), where r[i] is the distace bewteen the photon and each acceptors[i].
+        Returns the sum of the probability that a photon is transferred to the acceptor. Each probability has the form: (1 / tau_D) (R_0 / r [i]) ** 6 where r [i] is the distance between the photon and each acceptor and R_0 Forster radius.
+
+        Returns
+        -------
+        prob : float
+             Probability that a photon is transferred to the acceptor.
         """
-        self.dist = np.zeros(self.np.n_acceptors)
-        for i in range(self.np.n_acceptors):
-            self.dist[i] = (sum((self.photon - self.np.acceptors_positions[i])**2))**3
+        dist = np.zeros(self.NP.n_acceptors)
+        for i in range(self.NP.n_acceptors):
+            dist[i] = (self.photon - self.NP.acceptors_positions[i])**3
+
+        dist = 1/dist
+
+        cte = self.NP.R_Forster**6/self.NP.tau_D
+        prob = 1 - e**(self.np.delta_t * cte*sum(dist))
+
+    
