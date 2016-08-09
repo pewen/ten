c12ab4c1125f601140a8d25ff5d0772eb514ec06
franco bellomo <fnbellomo@hal>
2016-08-09 16:59:33 -0300

    solve for len(r_param) == 1

--- ten/core/nanoparticle.py
+++ ten/core/nanoparticle.py
@@ -44,8 +44,8 @@ class Nanoparticle(object):
         # NanoParticle paremeters
         if isinstance(r_param, (int, float)):
             r_param = [r_param, 0]
-        elif r_param.shape == 1:
-            r_param = [r_param, 0]
+        elif len(r_param) == 1:
+            r_param = [r_param[0], 0]
 
         self.r_param = r_param
 
