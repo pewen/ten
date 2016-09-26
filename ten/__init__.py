ac7bf8c4882ec433933e879adb6e56dc900464fb
fnbellomo <fnbellomo@gmail.com>
2016-09-26 13:55:59 -0300

    change name imports

--- ten/__init__.py
+++ ten/__init__.py
@@ -2,16 +2,31 @@ from __future__ import absolute_import
 
 from .core.nanoparticle import Nanoparticle
 from .core.aceptor import Aceptor
-from .experiments import experiments
-from .experiments.mechanisms import forster, boolean
+from . import experiments
+from .experiments import mechanisms
 
-#from .utils import generate_random_points_in_sphere, read4file, save_out
+# from .utils import generate_random_points_in_sphere, read4file, save_out
 
 __all__ = ['Nanoparticle',
            'Aceptor',
            'experiments',
-           'forster',
-           'boolean']
+           'mechanisms']
 
-# Alpha Release
-__version__ = '0.1.0a1'
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# Admissible pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+__version__ = '0.1.dev0'
