b5c8b1deb01b68bf1f55ea31ad05329797742d52
Lucas Bellomo <lbellomo@gmail.com>
2016-10-14 10:33:20 -0300

    removido los3

--- compile_f90.sh
+++ compile_f90.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd ten/random
-f2py3 -c generator.f90 -m generator
+f2py -c generator.f90 -m generator
 
 cd ../mechanisms
 gfortran -c -fPIC -O3 ../random/generator.f90 -o generator.o
-f2py3 -c --fcompiler=gfortran -I. generator.o -m forster_mecha forster_mecha.f90
+f2py -c --fcompiler=gfortran -I. generator.o -m forster_mecha forster_mecha.f90
